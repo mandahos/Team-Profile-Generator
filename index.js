d45b80c841379a29ee9cab21d95bec1cdba049ee
Amanda Hoshor <hoshor.manda@gmail.com>
2021-05-12 22:34:40 -0600

    added html and css frameworks

---
+++ index.js
@@ -0,0 +1,17 @@
+// pull the team info based on .js files for each category?
+
+function pullEmployee() {
+
+}
+
+function pullEngineer() {
+
+}
+
+function pullIntern() {
+
+}
+
+function pullManager() {
+
+}
