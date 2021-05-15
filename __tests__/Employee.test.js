5bb8ac8b0efd81688a2e85f5955829ea799be932
Amanda Hoshor <hoshor.manda@gmail.com>
2021-05-14 23:19:37 -0600

    added js to pull data, test is failing? tutoring in morning

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,14 +5,20 @@ const Employee = require('../lib/Employee');
 
 // console.log(new Employee());
 
-// test('checks to see employee info entered', () =>{
-//     const employee = new Employee('Jane');
+test('checks to see employee info entered', () =>{
+    const employee = new Employee('Jane');
 
-//     expect(employee.name).toBe('Jane');
-//     expect(employee.id).toBe(expect.any(Number));
-//     expect(employee.id).toBe(expect.any(String));
-//     expect(employee.id).toBe(expect.any(String));
-// });
+    expect(typeof(employee)).toBe("object");
+    // expect(employee.id).toBe(expect.any(Number));
+    // expect(employee.id).toBe(expect.any(String));
+    // expect(employee.id).toBe(expect.any(String));
+});
+
+test('checks the value of employee ID', () => {
+    const id = 256;
+    const employee = new Employee("Ed", id);
+    expect(employee.id).toBe(id);
+});
 
 test('checks to see employee has required information', () => {
  const employee = new Employee('Jim', '#256', 'jim@employee.com', 'employee');
--- index.js
+++ index.js
@@ -1,19 +1,43 @@
+//systems
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // pull the team info based on .js files for each category?
-let employee = new Employee('Jim', '#256', 'jim@employee.com', 'employee');
+const Employee = require('../lib/Employee'); 
+const Engineer = require ('../lib/Engineer');
+const Intern = require('../lib/Intern');
+const Manager = require('../lib/Manager');
+const { start } = require("repl");
 
+//pull in Team through command line
 
-function pullEmployee() {
+const team = [];
 
-}
+function startTeam() {
+    inquirer.prompt([{
+        message= "Enter team member's name",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select the team member's role",
+        choices: [
+            "Employee",
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    },
+    {
+        message: "Enter the team member's id number",
+        name: "id"
+    },
+    {
+        message: "Enter the team member's email address",
+        name: "email"
+    }
+   ])
 
-function pullEngineer() {
-
-}
-
-function pullIntern() {
-
-}
-
-function pullManager() {
 
 }
